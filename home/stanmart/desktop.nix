f5b16f140e205db436f5580c032f8feed0ede4e3
Martin Stancsics <martin.stancsics@quantco.com>
2026-01-20 09:23:03 +0100

    Git settings

--- home/stanmart/desktop.nix
+++ home/stanmart/desktop.nix
@@ -12,6 +12,14 @@
     vscode
   ];
 
+  # Git signing with GPG
+  programs.git = {
+    signing = {
+      signByDefault = true;
+      key = null; # Uses default GPG key
+    };
+  };
+
   # Desktop-specific settings can go here
   # For example: gtk themes, fonts, etc.
 }
