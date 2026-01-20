b2ed424d468e1145a6ee2ce0f116cfacf1034634
Martin Stancsics <martin.stancsics@quantco.com>
2026-01-20 22:03:25 +0100

    Nicer fallback for orbstack

--- hosts/orbstack/fallback.nix
+++ hosts/orbstack/fallback.nix
@@ -4,26 +4,19 @@
 { lib, pkgs, ... }:
 
 {
-
   boot.isContainer = lib.mkDefault true;
-
-  fileSystems."/" = lib.mkDefault {
-    device = "tmpfs";
-    fsType = "tmpfs";
-    options = [ "mode=0755" ];
-  };
 
   # Match orbstack's default user setup
   users.users.stanmart = {
-    uid = 501;
-    isSystemUser = true;
-    group = "users";
-    createHome = true;
-    home = "/home/stanmart";
-    homeMode = "700";
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
+    uid = lib.mkDefault 501;
+    extraGroups = lib.mkDefault [ "wheel" "orbstack" ];
+    isSystemUser = lib.mkDefault true;
+    group = lib.mkDefault "users";
+    createHome = lib.mkDefault true;
+    home = lib.mkDefault "/home/stanmart";
+    homeMode = lib.mkDefault "700";
+    useDefaultShell = lib.mkDefault true;
+    openssh.authorizedKeys.keys = lib.mkDefault [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICUXDcdw+FfLyMYNWmKs/j0LPAI4N29QzRJr92eR0vmK"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOPnYdF6Hbom3qxjSiM6mzXA6Luv5SB8N4v2axhgoYnvAAAABHNzaDo="
     ];
